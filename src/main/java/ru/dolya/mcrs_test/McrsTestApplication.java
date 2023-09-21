3fa84559117efcd5af041f6b774184ae611763be
Alexey Dolya <alexdolya90@gmail.com>
2023-09-21 18:45:23 +0400

    initial commit

---
+++ src/main/java/ru/dolya/mcrs_test/McrsTestApplication.java
@@ -0,0 +1,13 @@
+package ru.dolya.mcrs_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McrsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McrsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dolya/mcrs_test/McrsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dolya.mcrs_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McrsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
